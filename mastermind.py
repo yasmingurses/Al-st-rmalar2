e2fb7a4cf03679ed7fc56aa157006978900c5987
Yasmin Gürses <72697857+yasmingurses@users.noreply.github.com>
2021-02-03 00:41:30 +0300

    Update mastermind.py

--- mastermind.py
+++ mastermind.py
@@ -1,24 +1,23 @@
-#Kendi tanım aralığıma uyan random fonksyionu yaratma
-# 10 ve 98 dahil iki basamağı aynı olan sayılar hariç
-
 import random
-def yenirandom():
-  haric=[11,22,33,44,55,66,77,88]
-  aralik = randint(10,98)
-  return yenirandom() if aralik in haric else aralik 
+a = random.randrange(10,99)
+while a%11 ==0 :
+    a= random.randrange(10,99)
 
-#Kullanıcıdan sayı isteme
-dogruyer = 0
-x = int(input("Tahmin ettiğin sayıyı gir: "))
+x = (input("10 ile 98 arası bir sayı giriniz: "))
 
-#Burada işler karıştı soruya tekrar bak
+dogruyer= 0
+yanlısyer= 0
 
-
-while 10 <= x <= 98 and x[0] != x[1] :
-    if x[0] == int(yenirandom()[0]) or x[1] == int(yenirandom()[1]) :
+if int(x)<10 or int(x)>98 or int(x)%11 == 0:
+    print("Düzgün sayı gir!")
+    x = (input("10 ile 98 arası bir sayı giriniz: "))
+else:
+    if str(x)[0]== str(a)[0] or str(x)[1]== str(a)[1]:
         dogruyer = dogruyer + 1
-    else :
-        dogruyer = dogruyer - 1
+    elif str(x)[0]==str(a)[1] or str(x)[1]==str(a)[0]:
+        yanlısyer = yanlısyer + 1
+print("Sayı şuydu: ", a)
+print("Tahmin ettiğiniz sayı: {}".format(dogruyer))
+print("Tahmin edemediginiz sayı: {}".format(yanlısyer))
 
-print("Nizami sayı girmelisin!")
-print("Sonuç: " , dogruyer)
+
